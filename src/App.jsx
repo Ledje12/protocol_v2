5a9f7ef0896c6b56e8e170e94c6b0fc8023dd6da
Lejeune <jerome.lejeune@sace.be>
2026-09-13 14:54:11 +0200

    Polish library filters

--- src/App.jsx
+++ src/App.jsx
@@ -974,7 +974,7 @@ function SettingsScreen({ navigate }) {
             permission !== "denied" && (
               <div
                 style={{
-                  marginTop: "22px",
+                  marginTop: "30px",
                 }}
               >
                 <span
