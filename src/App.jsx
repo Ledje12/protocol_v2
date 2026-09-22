db1d1af580d59cd77d3097f3ab93a60dc76d36de
Ledje12 <jeromelejeune12@gmail.com>
2026-09-22 09:21:10 +0200

    Enable persistent Supabase auth sessions

--- src/App.jsx
+++ src/App.jsx
@@ -47,9 +47,9 @@ const supabase =
     supabaseKey,
     {
       auth: {
-        persistSession: false,
-        autoRefreshToken: false,
-        detectSessionInUrl: false,
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
       },
     }
   );
